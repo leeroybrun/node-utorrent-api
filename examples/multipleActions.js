17511d2e5ccadb4ac0405a4ea5521d125ecef1cb
Leeroy Brun <leeroy.brun@gmail.com>
2014-02-08 10:28:51 +0100

    Fix retryCallback error (thanks Luis) and add a multiple calls example

---
+++ examples/multipleActions.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var Client = require('../lib/utorrent');
+var fs = require('fs');
+
+var utorrent = new Client('localhost', '22222');
+utorrent.setCredentials('admin', '123456');
+
+// Add a new torrent
+request({'uri' : 'http://releases.ubuntu.com/13.04/ubuntu-13.04-desktop-i386.iso.torrent', 'encoding': null}, function (error, response, torrentFileBuffer) {
+	utorrent.call('add-file', {'torrent_file': torrentFileBuffer}, function(err, data) {
+		if(err) { console.log('error : '); console.log(err); return; }
+
+		console.log('Successfully added torrent file !');
+		console.log(data);
+
+		// List existing torrents
+		utorrent.call('list', function(err, data) {
+			//if(err) { console.log(err); return; }
+
+			console.log(data);
+		});
+	});
+});
